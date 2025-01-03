8cae8265f11ae311c0b45d6d376c77790dc50f1c
Matthew Borden <matthew.b@buildkite.com>
2025-01-03 14:04:10 +1100

    Use full path to ipa

--- .buildkite/scripts/bazel_build.sh
+++ .buildkite/scripts/bazel_build.sh
@@ -6,9 +6,14 @@ echo ":bazel: Building FlappyKite with Bazel"
 bazelisk build //FlappyKite:FlappyKite
 
 TOKEN=$(buildkite-agent oidc request-token --audience "https://packages.buildkite.com/buildkite/flappykite" --lifetime 300)
-FILE=$(bazelisk cquery //FlappyKite:FlappyKite --output=files --ui_event_filters=-info --noshow_progress)
 
-echo ":arrow_up: Uploading package to Buildkite Packages"
+FILE=$(bazelisk cquery //FlappyKite:FlappyKite --output=files --ui_event_filters=-info --noshow_progress)
+FILE=$(realpath $FILE)
+if [ ! -f "$FILE" ]; then
+    echo "Error: File $FILE does not exist."
+    exit 1
+fi
+echo ":arrow_up: Uploading IPA to Buildkite Packages"
 echo "IPA file: $FILE"
 curl -X POST https://api.buildkite.com/v2/packages/organizations/buildkite/registries/flappykite/packages \
      -H "Authorization: Bearer $TOKEN" \

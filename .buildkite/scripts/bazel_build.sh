04e1159261f7eed27d933ce3412d695100e1a0c6
Matthew Borden <matthew.b@buildkite.com>
2025-01-03 14:33:46 +1100

    Copy to tmp

--- .buildkite/scripts/bazel_build.sh
+++ .buildkite/scripts/bazel_build.sh
@@ -8,13 +8,10 @@ bazelisk build //FlappyKite:FlappyKite
 TOKEN=$(buildkite-agent oidc request-token --audience "https://packages.buildkite.com/buildkite/flappykite" --lifetime 300)
 
 FILE=$(bazelisk cquery //FlappyKite:FlappyKite --output=files --ui_event_filters=-info --noshow_progress)
-FILE=$(realpath $FILE)
-if [ ! -f "$FILE" ]; then
-    echo "Error: File $FILE does not exist."
-    exit 1
-fi
+
+cp $FILE /tmp/FlappyKite.ipa
 echo ":arrow_up: Uploading IPA to Buildkite Packages"
 echo "IPA file: $FILE"
 curl -X POST https://api.buildkite.com/v2/packages/organizations/buildkite/registries/flappykite/packages \
      -H "Authorization: Bearer $TOKEN" \
-     -F "file=@$FILE"
+     -F "file=@/tmp/FlappyKite.ipa"
